cd8edbbfc2e2fea99ccbfa965cac395349bb21ff
kenedyLima <kenedytorreslima@gmail.com>
2022-08-21 17:35:20 -0300

    adding tech logos list

--- src/components/AppContent.jsx
+++ src/components/AppContent.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import typingGif from "../images/R.gif";
 import myselfPhoto from "../images/me.jpg";
 
-import { LanguageOutline } from "react-ionicons";
+// import { LanguageOutline } from "react-ionicons";
 function AppContent() {
   return (
     <div className="container">
       <header className="main-header">
-        <span className="doc-section-text">Introduction</span>
+        <span className="doc-section-text" id="first-doc-section-text">
+          Introduction
+        </span>
         <div className="language-box">
-          <LanguageOutline width="20px" color="#f0f9ff" />
+          {/* <LanguageOutline width="20px" color="#f0f9ff" /> */}
           <span>Language</span>
         </div>
         <nav className="nav-bar">
@@ -19,7 +21,7 @@ function AppContent() {
               <a href="#about">About</a>
             </li>
             <li>
-              <a href="#tech">Tech</a>
+              <a href="#skills">Tech</a>
             </li>
             <li>Projects</li>
             <li>Contacts</li>
@@ -63,74 +65,81 @@ function AppContent() {
           {/* <img className="myself-photo" src={myselfPhoto} alt="" ></img> */}
         </div>
       </section>
-      <section className="tech-section" id="tech">
+      <section className="skills-section" id="skills">
         <div className="section-separator"></div>
-        <h2>Technologies</h2>
-        <p>List of languages and frameworks most used by me right now</p>
-        <ul>
+        <span className="doc-section-text">Skills</span>
+        <h2>Skills</h2>
+        <ul className="skills-list">
           <li>
-            <img
-              align="center"
-              alt="javascript logo"
-              height="30"
-              width="40"
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"
-            ></img>
+            <div className="skill-desc-wrapper">
+              <img
+                className="skill-logo"
+                alt="javascript logo"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"
+              ></img>
+              <span>Javascript</span>
+            </div>
           </li>
           <li>
-            <img
-              align="center"
-              alt="java logo"
-              height="30"
-              width="40"
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"
-            ></img>
+            <div className="skill-desc-wrapper">
+              <img
+                className="skill-logo"
+                alt="java logo"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"
+              ></img>
+              <span>Java</span>
+            </div>
           </li>
           <li>
-            <img
-              align="center"
-              alt="spring logo"
-              height="30"
-              width="40"
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg"
-            ></img>
+            <div className="skill-desc-wrapper">
+              <img
+                className="skill-logo"
+                alt="spring logo"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg"
+              ></img>
+              <span>Spring</span>
+            </div>
           </li>
           <li>
-            <img
-              align="center"
-              alt="myself logo"
-              height="30"
-              width="40"
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
-            ></img>
+            <div className="skill-desc-wrapper">
+              <img
+                className="skill-logo"
+                alt="myself logo"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
+              ></img>
+              <span>MySQL</span>
+            </div>
           </li>
           <li>
-            {" "}
-            <img
-              align="center"
-              alt="firebase logo"
-              height="30"
-              width="40"
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"
-            ></img>
+            <div className="skill-desc-wrapper">
+              <img
+                className="skill-logo"
+                alt="firebase logo"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"
+              ></img>
+              <span>Firebase</span>
+            </div>
+          </li>
+
+          <li>
+            <div className="skill-desc-wrapper">
+              <img
+                className="skill-logo"
+                alt="typescript logo"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-plain.svg"
+              ></img>
+              <span>TypeScript</span>
+            </div>
           </li>
           <li>
-            <img
-              align="center"
-              alt="typescript logo"
-              height="30"
-              width="40"
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-plain.svg"
-            ></img>
-          </li>
-          <li>
-            <img
-              align="center"
-              alt="react logo"
-              height="30"
-              width="40"
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg"
-            ></img>
+            <div className="skill-desc-wrapper">
+              <img
+                className="skill-logo"
+                alt="react logo"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg"
+              ></img>
+              <span>React</span>
+            </div>
           </li>
         </ul>
       </section>
